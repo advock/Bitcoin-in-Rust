a26ef2166b5a83c36c52d3a0c36e613bca99d3c4
ADVK <advaiyakarnik@gmail.com>
2022-12-21 22:49:55 +0530

    5th

--- advaiya-currency/src/main.rs
+++ advaiya-currency/src/main.rs
@@ -2,4 +2,8 @@ use blockchainlib::*;
 fn main() {
     let block = Block::new(0, 0, vec![0; 32], 0, "Genesis block".to_owned());
     println!("{:?}", &block);
+
+    let h = block.hash();
+
+    print!("{:?}", &h);
 }
